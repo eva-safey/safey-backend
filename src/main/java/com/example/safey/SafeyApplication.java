000af72722568fff98651122390580a0de391b38
ccarazasc <u20181b541@upc.edu.pe>
2022-05-31 14:30:09 -0500

    Initial commit

---
+++ src/main/java/com/example/safey/SafeyApplication.java
@@ -0,0 +1,13 @@
+package com.example.safey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeyApplication.class, args);
+    }
+
+}
